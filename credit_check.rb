c4c292b2758e9407d877792f0d7bfa1dde5bd518
ccorrigan <barnumswinter@gmail.com>
2017-03-19 10:45:04 -0600

    Reformatted comments for readability

--- credit_check.rb
+++ credit_check.rb
@@ -1,15 +1,15 @@
-# CreditCheck class performs Luhn Algorithm
 class CreditCheck
+# CreditCheck class performs Luhn Algorithm  
   
-  # This function accepts a credit card number and calls the main program function
   def initialize(card_number) 
+    # This method accepts a credit card number and calls the main program function
     @card_number = card_number
     is_valid?
   end
-
-  # This is the main function of the program: Formats number, calculates validity, 
-  # and prints the result 
+ 
   def is_valid?
+    # This is the main method of the program: Formats number, calculates validity, 
+    # and prints the result
     @card_number = format_card_number(@card_number)
     @card_number = double_odd_digits(@card_number)
     @card_number = sum_digits_over_10(@card_number)
@@ -17,16 +17,16 @@ class CreditCheck
     check_validity(@card_number) 
   end
 
-  # This function splits card number into digits, converts from string to integers, 
-  # and reverses the order of the digits
   def format_card_number(card_string)
+    # This method splits card number into digits, converts from string to integers, 
+    # and reverses the order of the digits
     card_string.reverse.split("").map! do |char|
       char.to_i
     end
   end 
 
-  # This function doubles the value of every other digit, beginning from the check digit
   def double_odd_digits(numbers_to_double)
+    # This method doubles the value of every other digit, beginning from the check digit
     doubles = []
     numbers_to_double.each_with_index do |num, index|
       num *= 2 if index.odd?
@@ -35,9 +35,9 @@ class CreditCheck
     return doubles
   end
 
-  # This function sums the individual digits of any numbers greater than 9 in the card 
-  # number array
   def sum_digits_over_10(products_to_sum)
+    # This method sums the individual digits of any numbers greater than 9 in the card 
+    # number array
     products_to_sum.map do |value|
       if value > 9
         value -= 9
@@ -47,15 +47,15 @@ class CreditCheck
     end
   end
 
-  # This function sums together all digits in the card number array
   def sum_all_digits(digits_to_sum)
+    # This method sums together all digits in the card number array
     digits_to_sum.reduce(0) do |sum, num|
       sum + num
     end
   end
 
-  # This function checks for even division by ten and prints result
   def check_validity(processed_number)
+    # This method checks for even division by ten and prints result
     if processed_number % 10 == 0
       valid = true
       puts "The number is valid!"
